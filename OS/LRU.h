62e010028763e5182637d0f20b3b2c60bd879b53
LoganMa <rookielogan8@gmail.com>
2021-06-16 23:44:26 +0800

    2021/6/16

--- OS/LRU.h
+++ OS/LRU.h
@@ -7,6 +7,8 @@
 
 namespace First
 {
+//put opterator:O(n)
+//get opterator:O(n)
 class LRU
 {
 private:
@@ -15,18 +17,23 @@ private:
         int key, val;
         Node(int _key=0,int _val=0):key(_key),val(_val){}
     };
+    typedef std::list<struct Node> Nodelist;
     std::list<struct Node> lnode;
+    int size;
+    
+    //some private help function
+    Nodelist::iterator find(int );
+    void removeLast();
+    void moveTofront(Nodelist::iterator &);
 
 public:
-    std::list<struct Node>::iterator find(int );
-    void removeLast();
-    void moveTofront(struct Node&);
+    LRU(int _size=MAXSIZE):size(_size){}
     void put(int,int);
     int get(int);
 };//class LRU
 
 
-std::list<struct LRU::Node>::iterator LRU::find(int _key)
+LRU::Nodelist::iterator LRU::find(int _key)
 {
     for(auto i = lnode.begin();i!=lnode.end();i++)
     {
@@ -45,11 +52,12 @@ void LRU::removeLast()
 }
 
 
-void LRU::moveTofront(struct Node & pnode)
+void LRU::moveTofront(Nodelist::iterator & _iter)
 {
-    struct Node temp = pnode;
-    lnode.remove(pnode);
-    put(temp.key,temp.val);
+    int key = _iter->key;
+    int val = _iter->val;
+    lnode.remove(*_iter);
+    put(key,val);
 }
 
 
@@ -58,26 +66,100 @@ void LRU::put(int key,int val)
     auto iter = find(key);
     if(iter!=lnode.end())
     {
-        moveTofront(*iter);
+        moveTofront(iter);
         return ;
     }   
-    if(lnode.size()>=MAXSIZE)
+    if(lnode.size()>=size)
     {
         removeLast();
     }
-    put(key,val);
+    lnode.push_front(Node(key,val));
 }
 
 
 int LRU::get(int _key)
 {
-    for(auto i = lnode.begin();i!=lnode.end();i++)
+    auto iter = find(_key);
+    if(iter!=lnode.end())
     {
-        if(i->key==_key) return i->val;
+        moveTofront(iter);
+        return lnode.front().val;
     }
     return -1;
 }
+}//namespace
 
+
+namespace Second
+{
+//put opterator:O(1)
+//get opterator:O(1)
+class LRU
+{
+    struct Node 
+    {
+        int key,val;
+        Node(int _key = 0,int _val=0):key(_key),val(_val){}
+        
+    };
+    typedef std::list<struct Node> Nodelist;
+    std::unordered_map<int,Nodelist::iterator>hashmap;
+    std::list<struct Node> lnode;
+    int size;
+
+    //private help function
+    void remove(Nodelist::iterator &);
+    void add(int,int);
+public:
+    LRU(int _size = MAXSIZE):size(_size){}
+    void put(int,int);
+    int get(int );
+};
+
+
+void LRU::remove(Nodelist::iterator & _iter)
+{
+    int key = _iter->key;
+    int val = _iter->val;
+    hashmap.erase(key);
+    lnode.erase(_iter);
+}
+
+void LRU::add(int _key,int _val)
+{
+    lnode.push_front(Node(_key,_val));
+    hashmap[_key] = lnode.begin();
+    if(lnode.size()>size)
+    {
+        auto end = lnode.end();
+        remove(--end);
+    }
+}
+
+
+void LRU::put(int _key,int _val)
+{
+    auto iter = hashmap.find(_key);
+    if(iter!=hashmap.end())
+    {
+        remove(iter->second);
+    }
+    add(_key,_val);
+}
+
+
+int LRU::get(int _key)
+{
+    auto iter = hashmap.find(_key);
+    if(iter!=hashmap.end())
+    {
+        int temp = iter->second->val;
+        remove(iter->second);
+        add(_key,temp);
+        return temp;
+    }
+    return -1;
+}
 }//namespace
 
 
